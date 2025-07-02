bf57e200106bebd286eb2797faa725a09d542f99
SkullGamer205 <aroslavlevkin86@gmail.com>
2025-07-02 19:54:06 +1000

    Added "Notification" Module

---
+++ river/unnamed/home/user/.config/river/modules/AstalWidgets/main.lua
@@ -0,0 +1,19 @@
+pcall(require, "luarocks.loader")
+local src = require("lib").src
+local astal = require("astal")
+local App = require("astal.gtk3").App
+local Bar = require("TopBar.Bar")
+local Notif = require("Notifications.init")
+
+local scss = src("style.scss")
+local css = "/tmp/astal-style.css"
+astal.exec("sass ".. scss .. " " .. css)
+App:start {
+    instance_name = "astal-river",
+    class_name = "astal-river",
+    css = css,
+    main = function()
+            Bar(monitor)
+            Notif(monitor)
+    end
+}
