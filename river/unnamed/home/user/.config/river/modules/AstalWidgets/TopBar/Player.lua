32304f46a123167423247cde2e1158f653014d34
SkullGamer205 <aroslavlevkin86@gmail.com>
2025-07-13 23:17:59 +1000

    Updated Player widget. Added function to call PlayerPopup

--- river/unnamed/home/user/.config/river/modules/AstalWidgets/TopBar/Player.lua
+++ river/unnamed/home/user/.config/river/modules/AstalWidgets/TopBar/Player.lua
@@ -1,4 +1,5 @@
 local astal = require("astal")
+local App = require("astal.gtk3").App
 local Widget = require("astal.gtk3.widget")
 local bind = astal.bind
 local Mpris = astal.require("AstalMpris")
@@ -8,26 +9,41 @@ local map = require("lib").map
 return function()
 	local player = Mpris.Player.new("mpd")
 
-	return Widget.Box({
-		class_name = "Media",
-		visible = bind(player, "available"),
-		Widget.Box({
-			class_name = "Cover",
-			valign = "CENTER",
-			css = bind(player, "cover-art"):as(
-				function(cover)
-					return "background-image: url('" .. (cover or "") .. "');"
-				end
-			),
-		}),
-		Widget.Label({
-			label = bind(player, "metadata"):as(
-				function()
-					return (player.title or "")
-						.. " - "
-						.. (player.artist or "")
-				end
-			),
-		}),
-	})
+	return Widget.Button({
+            name = "Player",
+            class_name = "player-button",
+            on_clicked = function()
+                local player_popup = App:get_window("MediaPlayerWindow")
+                if player_popup then
+                    if not player_popup:get_visible() then
+                        player_popup:show()
+                    else
+                        player_popup:hide()
+                    end
+                end
+            end,
+
+            Widget.Box({
+	            class_name = "Media",
+		    visible = bind(player, "available"),
+		    Widget.Box({
+			    class_name = "Cover",
+			    valign = "CENTER",
+			    css = bind(player, "cover-art"):as(
+				    function(cover)
+					    return "background-image: url('" .. (cover or "") .. "');"
+				    end
+			    ),
+		    }),
+		    Widget.Label({
+			    label = bind(player, "metadata"):as(
+				    function()
+					    return (player.title or "")
+						    .. " - "
+						    .. (player.artist or "")
+				    end
+			    ),
+		    }),
+	    })
+        })
 end
