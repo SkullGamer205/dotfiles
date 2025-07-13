612e4498b94e08768d0ca1d1985f6336b10c8f2a
SkullGamer205 <aroslavlevkin86@gmail.com>
2025-07-13 23:15:28 +1000

    Update main.lua

--- river/unnamed/home/user/.config/river/modules/AstalWidgets/main.lua
+++ river/unnamed/home/user/.config/river/modules/AstalWidgets/main.lua
@@ -5,17 +5,19 @@ local App = require("astal.gtk3").App
 local Bar = require("TopBar.Bar")
 local Notif = require("Notifications.init")
 local Launcher = require("Launcher.init")
+local PlayerPopup = require("PlayerPopup.init")
 
 local scss = src("style.scss")
 local css = "/tmp/astal-style.css"
 astal.exec("sass ".. scss .. " " .. css)
 App:start {
-    instance_name = "astal-river",
+    instance_name = "astal",
     class_name = "astal-river",
     css = css,
     main = function()
             Bar(monitor)
             Notif(monitor)
             Launcher()
+            PlayerPopup()
     end
 }
