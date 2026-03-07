d9eec9c438e23b437c3aae5781c2b7dba0a19723
Akira25 <skullgamer205@disroot.org>
2026-03-08 00:44:00 +1000

    cwc: Fixed "tags" widget

--- lunatic/.config/cwc/module/astal-widgets/statusbar/tags/init.lua
+++ lunatic/.config/cwc/module/astal-widgets/statusbar/tags/init.lua
@@ -1,22 +1,30 @@
 local astal = require("astal")
 local Widget = require("astal.gtk3").Widget
 
-local cwc = cwc
-
 local function tag_button(tag)
     return Widget.Button({
-        name = "Tag" .. tag,
+       name = "Tag" .. tag,
         class_name = "button-tag",
-        t = cwc.screen.focused():get_tag(tag),
         on_click_release = function(_, event)
+        local t = string.format('cwc.screen.focused():get_tag(%s)', tag)
             if event.button == "PRIMARY" then
-                t:view_only()
+                os.execute('cwctl -c \"' .. t .. ':view_only()\"')
             elseif event.button == "SECONDARY" then
-                t:toggle()
+                os.execute('cwctl -c \"' .. t .. ':toggle()\"')
             else
                 print("NOTHING")
             end
         end,
+
+        Widget.Box({
+           name = "Tag" .. tag,
+            class_name = "box-tag",
+            Widget.Label({
+                class_name = "sting-tag",
+                label = string.format('%s', tag)
+            })
+        })
+
     })
 end
 
@@ -26,7 +34,8 @@ return function()
         _tag_button[i] = tag_button(i)
     end
 
-    Widget.Box({
+    return Widget.Box({
+        vertical = true,
         table.unpack(_tag_button),
     })
 end
