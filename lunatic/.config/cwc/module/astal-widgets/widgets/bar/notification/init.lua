9f0196dc32824c16bea13e467e125bb99f56c119
Akira25 <skullgamer205@disroot.org>
2026-03-13 22:05:25 +1000

    cwc(astal): Fix icon

--- lunatic/.config/cwc/module/astal-widgets/widgets/bar/notification/init.lua
+++ lunatic/.config/cwc/module/astal-widgets/widgets/bar/notification/init.lua
@@ -11,7 +11,7 @@ return function()
             end
         end,
         Widget.Icon({
-            icon = "notification-nactive",
+            icon = "notification-active",
         }),
     })
 end
