fd55d4ffada31334767af8b94d31b7ff31ced031
Akira25 <skullgamer205@disroot.org>
2026-04-18 03:30:36 +1000

    cwc(astal): Update 'Weather widget'. Add semiworking weather features (W.I.P.)

--- lunatic/.config/cwc/module/astal-widgets/windows/clock/init.lua
+++ lunatic/.config/cwc/module/astal-widgets/windows/clock/init.lua
@@ -1,13 +1,102 @@
+pcall(require, "luarocks.loader")
 local Widget    = require("astal.gtk3").Widget
 
 local lgi       = require('lgi')
 local GLib      = lgi.require('GLib', '2.0')
 local Gtk       = lgi.require('Gtk', '3.0')
 
+local http      = require("socket.http")
+local json      = require("dkjson")
+
 local Debug     = require("lib.debug")
 
-local function DateTime(format, ...)
 
+
+-- NOTE: NEED TO MAKE SEPARATE CONFIG.LUA/JSON FILE!!!
+local w_config = {
+    date        = GLib.DateTime.new_now_local():format("%Y-%m-%d"),
+    timezone    = "auto",
+    city        = "Khabarovsk",
+    -- latitude    = nil,
+    -- longitude   = nil,
+    daily       = {
+        "sunrise",
+        "sunset"
+    },
+    hourly      = {
+        "temperature_2m",
+        "weather_code",
+        "wind_speed_10m",
+        "wind_direction_10m"
+    },
+    current     = {
+        "temperature_2m",
+        "weather_code",
+        "apparent_temperature",
+        "relative_humidity_2m",
+        "wind_speed_10m",
+        "wind_direction_10m"
+    },
+}
+
+local function get_geo(w_args)
+    Debug.debug("WeatherWidget", "City name are set. Trying to get coordinates")
+
+    local site = "https://geocoding-api.open-meteo.com/v1/search?name=" .. w_args.city .. "&count=1&format=json"
+    local response_body, status_code = http.request(site)
+
+    if status_code == 200 then
+        local data, pos, err = json.decode(response_body)
+        
+        if not err then
+            Debug.debug("WeatherWidget_GetGeo", string.format("HTTP Request Success: %i", status_code))
+            w_args.latitude  = data.results[1].latitude
+            w_args.longitude = data.results[1].longitude
+            w_args.city      = data.results[1].name
+        else
+            Debug.error("WeatherWidget_GetGeo", string.format("JSON Decode Error: %s", err))
+        end
+    else
+        Debug.error("WeatherWidget_GetGeo", string.format("HTTP Request Failed: %i", status_code))
+    end
+end
+
+
+local function get_weather(w_args)
+    if w_args.latitude or w_args.longtude == nil then
+        if w_args.city then
+            Debug.warn("WeatherWidet", "Latitude and/or longture aren't set. Found 'city' variable. Trying to obtain coorditanes from city name")
+            get_geo(w_args)
+        else
+            Debug.error("WeatherWidet", "Latitude and/or longture aren't set. 'city' variable not found. Weather widget cannot work!")
+            return
+        end
+    end
+
+    local site = "https://api.open-meteo.com/v1/forecast?latitude=".. w_args.latitude .."&longitude=".. w_args.longitude .."&daily=".. table.concat(w_args.daily, ",") .."&hourly=" .. table.concat(w_args.hourly, ",") .."&current=" .. table.concat(w_args.current, ",") .."&timezone=" .. w_args.timezone .. "&start_date=" .. w_args.date .. "&end_date=" .. w_args.date
+
+    local response_body, status_code = http.request(site)
+
+    if status_code == 200 then
+        local data, pos, err = json.decode(response_body)
+
+        if not err then
+            Debug.debug("WeatherWidget", string.format("HTTP Request Success: %i", status_code))
+            return data 
+        else
+            Debug.error("WeatherWidget", string.format("JSON Decode Error: %s", err))
+        end
+    else
+        Debug.error("WeatherWidget", string.format("HTTP Request Failed: %i", status_code))
+    end
+end
+
+local weather_data = get_weather(w_config)
+
+
+
+
+local function DateTime(format, ...)
     local defines = {...}
     local success, datetime = pcall(function()
         local dt = GLib.DateTime.new_now_local()
@@ -84,7 +173,12 @@ function CurrentWindow.new(gdkmonitor)
 
                 on_click_release = function(_, event)
                     if event.button == "PRIMARY" then
-                        print(GLib.DateTime.new_now_local():add_days(i - 2):format("%Y-%m-%d"))
+                        -- print(GLib.DateTime.new_now_local():add_days(i - 2):format("%Y-%m-%d"))
+                        local selected_date = GLib.DateTime.new_now_local():add_days(i - 2):format("%Y-%m-%d")
+                        w_config.date = selected_date
+                        Debug.debug("WeatherWidget", string.format("Selected date: %s", selected_date))
+                        get_weather(w_config)
+
                     end
                 end
             })
@@ -118,7 +212,7 @@ function CurrentWindow.new(gdkmonitor)
                 }),
                 Widget.Label({
                     halign = "CENTER",
-                    label = "Москва",
+                    label = (w_config.city or "Неизвестно"),
                     css = "font-size: 100%; font-weight: 400;",
                 })
             })
@@ -140,10 +234,10 @@ function CurrentWindow.new(gdkmonitor)
                         vertical = true,
                         Widget.Label({
                             css = "font-size: 300%; font-weight: 600;",
-                            label = "0°C"
+                            label = ((weather_data.current.temperature_2m or "N/A") .. "°C")
                         }),
                         Widget.Label({
-                            label = "Облачно"
+                            label = ("Code: " .. (weather_data.current.weather_code or "N/A"))
                         })
                     }),
                 }),
@@ -156,7 +250,7 @@ function CurrentWindow.new(gdkmonitor)
                             icon = "nil",
                         }),
                         Widget.Label({
-                            label = "0%",
+                            label = ((weather_data.current.relative_humidity_2m or "N/A") .. "%")
                         })
                     }),
 
@@ -167,7 +261,7 @@ function CurrentWindow.new(gdkmonitor)
                             icon = "temperature-normal",
                         }),
                         Widget.Label({
-                            label = "0°C",
+                            label = ((weather_data.current.apparent_temperature or "N/A") .. "°C")
                         })
                     }),
 
@@ -178,7 +272,7 @@ function CurrentWindow.new(gdkmonitor)
                             icon = "weather-windy",
                         }),
                         Widget.Label({
-                            label = "0km/h",
+                            label = ((weather_data.current.wind_speed_10m or "N/A") .. "km/h")
                         })
                     }),
                 })
@@ -194,19 +288,19 @@ function CurrentWindow.new(gdkmonitor)
                     vertical = true,
                     class_name = "box",
                     Widget.Label({
-                        label = string.format("%d:00", i),
+                        label = string.format("%d:00", i - 1),
                     }),
                     Widget.Icon({
                         icon = "weather-clouds",
                     }),
                     Widget.Label({
-                        label = "0°C",
+                        label = ((weather_data.hourly.temperature_2m[i] or "N/A") .. "°C")
                     }),
                 })
             end
 
             for i = 1, 24 do 
-                h_forecasts[#h_forecasts + 1] = h_forecast(i - 1) 
+                h_forecasts[#h_forecasts + 1] = h_forecast(i) 
             end
 
             return Widget.Box({
@@ -230,6 +324,11 @@ function CurrentWindow.new(gdkmonitor)
                 current_weather(),
                 hourly_forecast(),
             },
+
+            setup = function(self)
+                w_config.date = GLib.DateTime.new_now_local():format("%Y-%m-%d")
+                return true
+            end,
         })
     end
 
