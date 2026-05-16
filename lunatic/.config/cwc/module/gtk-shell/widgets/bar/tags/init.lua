6544feb0bc6e4e700f773c9a62c573bc0bcdea32
Akira25 <skullgamer205@disroot.org>
2026-05-16 18:25:09 +1000

    gtk-shell: Update "TagButtons"
    Now it works. At this moment without css style

--- lunatic/.config/cwc/module/gtk-shell/widgets/bar/tags/init.lua
+++ lunatic/.config/cwc/module/gtk-shell/widgets/bar/tags/init.lua
@@ -2,24 +2,29 @@ local lgi  = require('lgi')
 local GLib = lgi.require('GLib')
 local Gtk  = lgi.require('Gtk', '3.0')
 
+-- Tag button template
 local tag_button = function(tag)
     local t_button = Gtk.Button({
         label = tag,
-        on_clicked = function(_, event)
-            local cmd   = string.format('cwc.screen.focused():get_tag(%s)', tag)
-            local dnull = string.format(' > /dev/null 2>1')
-            if event.button == "PRIMARY" then
-                os.execute('cwctl -c \"' .. cmd .. ':view_only()\"' .. dnull)
-            elseif event.button == "SECONDARY" then 
-                os.execute('cwctl -c \"' .. cmd .. ':toggle()\"' .. dnull)
-            end
-        end
     })
+
+    function t_button:on_button_release_event(event)
+        local cmd   = string.format('cwc.screen.focused():get_tag(%s)', tag)
+        local null = string.format(' > /dev/null 2>&1')
+        if event.button == 1 then   -- Left mouse click
+            os.execute('cwctl -c \"' .. cmd .. ':view_only()\"' .. null)
+        elseif event.button == 3 then    -- Right mouse click
+            os.execute('cwctl -c \"' .. cmd .. ':toggle()\"' .. null)
+        end
+    end
+
 
     return t_button
 end
 
+-- Box with all buttons
 local function TagBox()
+    -- Table&function to pack several buttons
     local _tag_button = {}
     for i = 1, 4 do
         _tag_button[i] = tag_button(i)
