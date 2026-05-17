07d220eb17fca39954afdf3a5398691db929a797
Akira25 <skullgamer205@disroot.org>
2026-05-18 04:17:13 +1000

    gtk-shell: Update

--- lunatic/.config/cwc/module/gtk-shell/windows/launcher/init.lua
+++ lunatic/.config/cwc/module/gtk-shell/windows/launcher/init.lua
@@ -2,11 +2,32 @@ local lgi = require('lgi')
 local Gtk = lgi.require('Gtk', '3.0')
 local LayerShell = lgi.require('GtkLayerShell')
 
-local launcher_label = Gtk.Label.new("Launcher")
+-- Entry
+local search_entry = Gtk.Entry({
+    placeholder_text = "Search..."
+})
+
+-- Button
+local clean_icon = Gtk.Image({
+    icon_name = 'edit-clear'
+})
+
+local entry_cleaner = Gtk.Button({
+    clean_icon,
+    on_clicked = function()
+        search_entry:set_text('')
+    end
+})
+
+-- Search panel
+local search_box = Gtk.Box({
+    search_entry,
+    entry_cleaner
+})
 
 local launcher_box = Gtk.Box({
         orientation = "VERTICAL",
-        launcher_label
+        search_box
     })
 
 local LauncherWindow = {}
