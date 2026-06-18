078479b98a3d081d9793d1705d14bd8bfc556d53
Akira25 <skullgamer205@disroot.org>
2026-06-18 20:38:15 +1000

    somewm: Move 'menu' block
    rc.lua -> ui/menu/init.lua

---
+++ lunatic/.config/somewm/ui/menu/init.lua
@@ -0,0 +1,26 @@
+local awful     = require('awful')
+local beautiful = require('beautiful')
+
+local apps = require('config.apps')
+local hk_popup = awful.require('hotkeys_popup')
+
+-- @DOC_MENU@
+-- Create a launcher widget and a main menu
+local menu = {}
+
+menu.somewm = {
+   { "hotkeys", function() hk_popup.show_help(nil, awful.screen.focused()) end },
+   { "manual", apps.terminal .. " -e man awesome" },
+   { "edit config", apps.editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", function() awesome.quit() end },
+}
+
+menu.main = awful.menu({
+    items = {
+        { "awesome", menu.somewm, beautiful.awesome_icon },
+        { "open terminal", apps.terminal }
+    }
+})
+
+return menu
