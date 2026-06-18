5f3a89202430490a77b10f365cdd9196c2c68d87
Akira25 <skullgamer205@disroot.org>
2026-06-19 00:02:55 +1000

    somewm: Move 'notification' block

---
+++ lunatic/.config/somewm/signal/naughty.lua
@@ -0,0 +1,18 @@
+local awful   = require('awful')
+local naughty = require('naughty')
+local ruled   = require('ruled')
+
+ruled.notification.connect_signal('request::rules', function()
+    -- All notifications will match this rule.
+    ruled.notification.append_rule {
+        rule       = { },
+        properties = {
+            screen           = awful.screen.preferred,
+            implicit_timeout = 5,
+        }
+    }
+end)
+
+naughty.connect_signal("request::display", function(n)
+    require('ui.notification')(n)
+end)
