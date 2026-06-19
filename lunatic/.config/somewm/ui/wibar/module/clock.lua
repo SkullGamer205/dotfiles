189e54d129c1585ffac418e2526fc32a90de9ca8
Akira25 <skullgamer205@disroot.org>
2026-06-20 01:50:30 +1000

    somewm: Remade 'clock' widget

--- lunatic/.config/somewm/ui/wibar/module/clock.lua
+++ lunatic/.config/somewm/ui/wibar/module/clock.lua
@@ -1,6 +1,32 @@
 local wibox = require('wibox')
+local gears = require('gears')
 
+local function Time(format)
+    -- Make a simple widget
+    local time_widget = wibox.widget({
+        widget  = wibox.widget.textbox,
+        align   = 'center',
+        valign  = 'center',
+        text    = '',
+    })
+
+    -- Update function
+    local function update_func()
+        time_widget:set_text('' .. os.date(format))
+    end
+
+    -- Timer to trigger update function
+    gears.timer({
+        timeout     = 1,
+        autostart   = true,
+        call_now    = true,
+        callback    = update_func,
+    })
+
+    -- return widget
+    return time_widget
+end
+    
 return function()
--- Create a textclock widget
-    return wibox.widget.textclock()
+    return Time('%H\n%M\n%S')
 end
