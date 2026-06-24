15f79634062b0e828a0d534d1bd1c401200fea81
Akira25 <skullgamer205@disroot.org>
2026-06-25 03:00:44 +1000

    somewm: Some user changes

--- lunatic/.config/somewm/config/user.lua
+++ lunatic/.config/somewm/config/user.lua
@@ -4,22 +4,23 @@ local awful = require('awful')
 return {
     modkey = "Mod4",
 
-    tags = { '1', '2', '3', '4', '5', '6', '7', '8' },
+    -- tags = { '1', '2', '3', '4', '5', '6', '7', '8' },
+    tags = { '1', '2', '3', '4'},
 
     layouts = {
-        awful.layout.suit.floating,
         awful.layout.suit.tile,
-        awful.layout.suit.carousel,
-        awful.layout.suit.tile.left,
-        awful.layout.suit.tile.bottom,
-        awful.layout.suit.tile.top,
-        awful.layout.suit.fair,
-        awful.layout.suit.fair.horizontal,
-        awful.layout.suit.spiral,
         awful.layout.suit.spiral.dwindle,
-        awful.layout.suit.max,
-        awful.layout.suit.max.fullscreen,
-        awful.layout.suit.magnifier,
-        awful.layout.suit.corner.nw,
+        awful.layout.suit.carousel,
+        awful.layout.suit.floating,
+        -- awful.layout.suit.tile.left,
+        -- awful.layout.suit.tile.bottom,
+        -- awful.layout.suit.tile.top,
+        -- awful.layout.suit.fair,
+        -- awful.layout.suit.fair.horizontal,
+        -- awful.layout.suit.spiral,
+        -- awful.layout.suit.max,
+        -- awful.layout.suit.max.fullscreen,
+        -- awful.layout.suit.magnifier,
+        -- awful.layout.suit.corner.nw,
     }
 }
