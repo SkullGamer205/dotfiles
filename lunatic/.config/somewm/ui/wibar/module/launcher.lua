9abad8975cb43d6ff5b93162797950d0b62ee88c
Akira25 <skullgamer205@disroot.org>
2026-06-25 14:06:45 +1000

    somewm: Update

--- lunatic/.config/somewm/ui/wibar/module/launcher.lua
+++ lunatic/.config/somewm/ui/wibar/module/launcher.lua
@@ -1,10 +1,26 @@
-local awful     = require('awful')
-local beautiful = require('beautiful')
+local awful      = require('awful')
+local beautiful  = require('beautiful')
+
+local SimpleWidgets = require('utils.simple_widgets')
+
+local icon      = SimpleWidgets.image(beautiful.launcher_icon, nil, beautiful.fg_normal, beautiful.palette.magenta)
+local widget    = SimpleWidgets.square(icon, beautiful.bg_normal)
+
+widget:add_button(awful,button({}, 1, function()
+    -- require('ui.menu').main
+end)
+)
+widget:add_button(awful.button({}, 1, function()
+end))
 
 return function()
--- Create a launcher widget
-    return awful.widget.launcher({
-        image = beautiful.awesome_icon,
-        menu  = require('ui.menu').main
-    })
+    return widget
 end
+
+-- return function()
+-- -- Create a launcher widget
+--     return awful.widget.launcher({
+--         image = beautiful.launcher_icon,
+--         menu  = require('ui.menu').main
+--     })
+-- end
