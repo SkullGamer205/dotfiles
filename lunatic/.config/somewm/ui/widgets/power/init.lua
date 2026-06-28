81e2345b9883e1827fceea47950f7902ecdeeaa0
Akira25 <skullgamer205@disroot.org>
2026-06-28 19:34:28 +1000

    somewm: Add options into 'power' widget

--- lunatic/.config/somewm/ui/widgets/power/init.lua
+++ lunatic/.config/somewm/ui/widgets/power/init.lua
@@ -5,7 +5,16 @@ local gears         = require('gears')
 local wibox         = require('wibox')
 local SimplePopup   = require('module.simple_widgets.smpl_popup')
 
-local function power_widget()
+local options = {
+    {name = 'Poweroff',     icon = beautiful.power_shutdown,    key = 'p',    command = 'loginctl poweroff'},
+    {name = 'Reboot',       icon = beautiful.power_reboot,      key = 'r',    command = 'loginctl reboot'},
+    {name = 'Log out',      icon = beautiful.power_logout,      key = 'o',    command = 'loginctl kill-session self'},
+    {name = 'Lock',         icon = beautiful.power_lockscreen,  key = 'l',    command = 'loginctl lock-session self'},
+    {name = 'Suspend',      icon = beautiful.power_suspend,     key = 's',    command = 'loginctl suspend'},
+    {name = 'Hibernate',    icon = beautiful.power_hibernate,   key = 'h',    command = 'loginctl hibernate'},
+}
+
+local function create_power_widget()
     return wibox.widget({
         widget  = wibox.container.background,
         {
@@ -27,7 +36,7 @@ local function power_widget()
 end
 
 local powermenu = SimplePopup.create('powermenu', {
-    main_widget = power_widget,
+    main_widget = create_power_widget,
     placement   = awful.placement.maximize,
 })
 
