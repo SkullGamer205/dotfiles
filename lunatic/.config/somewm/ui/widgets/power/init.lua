c45d4cd44d3cebe4292626a55c7de746274db99b
Akira25 <skullgamer205@disroot.org>
2026-06-29 19:25:05 +1000

    somewm: Update 'power' widget

--- lunatic/.config/somewm/ui/widgets/power/init.lua
+++ lunatic/.config/somewm/ui/widgets/power/init.lua
@@ -3,7 +3,9 @@ local awful         = require('awful')
 local beautiful     = require('beautiful')
 local gears         = require('gears')
 local wibox         = require('wibox')
-local SimplePopup   = require('module.simple_widgets.smpl_popup')
+local SimplePopup   = require('module.simple_widgets.popup').create
+local SimpleIcon    = require('module.simple_widgets.image').create_icon
+local SimpleBox     = require('module.simple_widgets.box').create_box
 
 local options = {
     {name = 'Poweroff',     icon = beautiful.power_shutdown,    key = 'p',    command = 'loginctl poweroff'},
@@ -14,7 +16,46 @@ local options = {
     {name = 'Hibernate',    icon = beautiful.power_hibernate,   key = 'h',    command = 'loginctl hibernate'},
 }
 
+local selected_index = 1
+
+local function create_button(option, index)
+    local is_selected = index == selected_index
+
+    local icon_widget = SimpleIcon(option.icon, {
+        main_color      = beautiful.fg_normal,
+        highlight_color = beautiful.bg_focus,
+    })
+
+    local text_widget = wibox.widget({
+        widget = wibox.widget.textbox,
+        text = '[' .. option.key .. '] ' .. option.name,
+    })
+
+    local widget = wibox.widget({
+        widget   = wibox.container.margin,
+        margins  = 16,
+        {
+            widget = wibox.layout.fixed.vertical,
+            spacing = 8,
+            icon_widget,
+            text_widget,
+        }
+    })
+
+    local button = SimpleBox(widget, {
+        main_color = beautiful.bg_normal
+    })
+
+    return button
+end
+
 local function create_power_widget()
+    local buttons = {}
+
+    for i, option in pairs(options) do
+        table.insert(buttons, create_button(option, i))
+    end
+
     return wibox.widget({
         widget  = wibox.container.background,
         {
@@ -30,12 +71,16 @@ local function create_power_widget()
                     halign  = 'center',
                     text    = 'Hello World',
                 },
+                {
+                    layout = wibox.layout.fixed.horizontal,
+                    table.unpack(buttons),
+                },
             },
         }
     })
 end
 
-local powermenu = SimplePopup.create('powermenu', {
+local powermenu = SimplePopup('powermenu', {
     main_widget = create_power_widget,
     placement   = awful.placement.maximize,
 })
