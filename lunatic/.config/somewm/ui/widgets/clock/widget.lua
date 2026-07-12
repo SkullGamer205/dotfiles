3e2bb86c1bd26cfcd363f35988634f0ab9d10d5e
SkullGamer205 <aroslavlevkin86@gmail.com>
2026-07-12 21:55:51 +1000

    somewm: Add calendar widget (W.I.P.).

--- lunatic/.config/somewm/ui/widgets/clock/widget.lua
+++ lunatic/.config/somewm/ui/widgets/clock/widget.lua
@@ -12,7 +12,7 @@ local function clock_widget(format)
         align   = 'center',
         valign  = 'center',
         text    = '',
-        font    = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$') * 2
+        font    = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$') * 4
     })
 
     local function update_func()
@@ -29,6 +29,51 @@ local function clock_widget(format)
     return clock_widget
 end
 
+local function calendar_widget()
+    local cal_buttons  = {}
+    local current_time = os.time()
+    local day          = 24 * 60 * 60
+
+    local current_month = wibox.widget({
+        widget = wibox.widget.textbox,
+        halign = 'center',
+        valign = 'center',
+        text   = os.date('%B. %Y', current_time + ((- 2) * day)),
+        font   = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$') * 1.5
+    })
+
+    local function cal_date_button(i)
+        local function text_widget(i)
+            return wibox.widget({
+                widget = wibox.widget.textbox,
+                halign = 'center',
+                valign = 'center',
+                text   = os.date('%d (%a)', current_time + ((- 2 + i) * day)),
+                font   = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$') * 1.25
+            })
+        end
+
+        return SimpleBox(text_widget(i), {
+            main_color          = beautiful.bg_normal,
+            highlight_color     = beautiful.bg_focus,
+            margin              = 2,
+        })
+    end
+
+    for i = 1, 7 do
+        cal_buttons[#cal_buttons + 1] = cal_date_button(i)
+    end
+
+    return wibox.widget({
+        widget = wibox.container.place,
+        {
+            layout  = wibox.layout.fixed.vertical,
+            current_month,
+            unpack(cal_buttons)
+        }
+    })
+end
+
 local function create_clock_widget()
     return wibox.widget({
         widget  = wibox.container.place,
@@ -42,7 +87,13 @@ local function create_clock_widget()
             {
                 layout  = wibox.layout.fixed.horizontal,
                 -- Clock
-                SimpleBox(clock_widget('%H\n%M\n%S'))
+                SimpleBox(clock_widget('%H\n%M\n%S'),
+                {
+                    -- main_color = beautiful.bg_focus,
+                    margin     = 8,
+                }
+            ),
+                calendar_widget(),
                 -- Weather
                 -- {},
             }
