8986f0f029daf1909a5388233009ebf8ca56c32e
SkullGamer205 <aroslavlevkin86@gmail.com>
2026-08-17 19:01:13 +1000

    somewm: Update binds
    Decrease local keys's code length

--- lunatic/.config/somewm/binds/client/keys.lua
+++ lunatic/.config/somewm/binds/client/keys.lua
@@ -1,51 +1,59 @@
-local awful = require('awful')
+local awful     = require('awful')
+local helpers   = require('binds.helpers') 
 
 local mod = require('binds.mod')
 local modkey = mod.modkey
 
+local local_helpers = {
+    toggle_fullscreen = function(c)
+        c.fullscreen = not c.fullscreen
+        c:raise()
+    end,
+
+    toggle_maximized = function(c)
+        c.maximized = not c.maximized
+        c:raise()
+    end
+    
+    toggle_maximized_v = function(c)
+        c.maximized_vertical = not c.maximized_vertical
+        c:raise()
+    end
+
+    toggle_maximized_h = function(c)
+        c.maximized_horizontal = not c.maximized_horizontal
+        c:raise()
+    end
+
+    minimize = function(c)
+        c.minimized = true
+    end
+
+    toggle_on_top = function(c)
+        c.ontop = not c.ontop
+    end
+}
+
 -- @DOC_CLIENT_KEYBINDINGS@
+local c_keys = {
+     -- Mod      / Key                            / Action                                                       / Description                   / Category
+     --{{        }, ""                            ,                                                              , ""                            , ""            },
+
+     -- Client
+     {{ modkey, mod.shift  }, "q"                 , function(c) c:kill() end                                     , "Close window"                , "Client"      },
+     {{ modkey,            }, "n"                 , local_helpers.minimize                                       , "Minimize"                    , "Client"      },
+     {{ modkey,            }, "m"                 , local_helpers.toggle_maximized                               , "(Un)maximize"                , "Client"      },
+     {{ modkey, mod.shift  }, "m"                 , local_helpers.toggle_maximized_h                             , "(Un)maximize horizontally"   , "Client"      },
+     {{ modkey, mod.ctrl   }, "m"                 , local_helpers.toggle_maximized_v                             , "(Un)maximize vertically"     , "Client"      },
+     {{ modkey,            }, "f"                 , local_helpers.toggle_fullscreen                              , "Toggle fullscreen"           , "Client"      },
+     {{ modkey,            }, "Space"             , awful.client.floating.toggle                                 , "Toggle floating"             , "Client"      },
+     {{ modkey,            }, "f"                 , local_helpers.toggle_fullscreen                              , "Toggle fullscreen"           , "Client"      },
+     {{ modkey,            }, "t"                 , local_helpers.toggle_on_top                                  , "Toggle keep on top"          , "Client"      },
+     {{ modkey, mod.ctrl   }, "Return"            , function(c) c:swap(awful.client.getmaster()) end             , "Move to master"              , "Client"      },
+     {{ modkey,            }, "o"                 , function(c) c:move_to_screen() end                           , "Move to screen"              , "Client"      },
+
+}
+
 client.connect_signal("request::default_keybindings", function()
-    awful.keyboard.append_client_keybindings({
-        awful.key({ modkey,           }, "f",
-            function (c)
-                c.fullscreen = not c.fullscreen
-                c:raise()
-            end,
-            {description = "toggle fullscreen", group = "client"}),
-        awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
-                {description = "close", group = "client"}),
-        awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
-                {description = "toggle floating", group = "client"}),
-        awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end,
-                {description = "move to master", group = "client"}),
-        awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
-                {description = "move to screen", group = "client"}),
-        awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-                {description = "toggle keep on top", group = "client"}),
-        awful.key({ modkey,           }, "n",
-            function (c)
-                -- The client currently has the input focus, so it cannot be
-                -- minimized, since minimized clients can't have the focus.
-                c.minimized = true
-            end ,
-            {description = "minimize", group = "client"}),
-        awful.key({ modkey,           }, "m",
-            function (c)
-                c.maximized = not c.maximized
-                c:raise()
-            end ,
-            {description = "(un)maximize", group = "client"}),
-        awful.key({ modkey, "Control" }, "m",
-            function (c)
-                c.maximized_vertical = not c.maximized_vertical
-                c:raise()
-            end ,
-            {description = "(un)maximize vertically", group = "client"}),
-        awful.key({ modkey, "Shift"   }, "m",
-            function (c)
-                c.maximized_horizontal = not c.maximized_horizontal
-                c:raise()
-            end ,
-            {description = "(un)maximize horizontally", group = "client"}),
-    })
+  awful.keyboard.append_client_keybindings(table_to_keybinding(c_keys))
 end)
