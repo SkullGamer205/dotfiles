3fb67ede7868e80b4bbfac6b6d38a6d9a033b634
SkullGamer205 <aroslavlevkin86@gmail.com>
2026-08-21 20:21:35 +1000

    somewm: Update 'menu'
    Integrate 'freedesktop' module. General update.

--- lunatic/.config/somewm/ui/menu/init.lua
+++ lunatic/.config/somewm/ui/menu/init.lua
@@ -3,24 +3,56 @@ local beautiful = require('beautiful')
 
 local apps = require('config.apps')
 local hk_popup = require('awful.hotkeys_popup')
+local freedesktop = require('module.freedesktop')
 
 -- @DOC_MENU@
 -- Create a launcher widget and a main menu
 local menu = {}
 
+menu.placeholder = { " ─────────── " }
+
 menu.somewm = {
-   { "hotkeys", function() hk_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", apps.terminal .. " -e man awesome" },
-   { "edit config", apps.editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
+   { "Hotkeys", function() hk_popup.show_help(nil, awful.screen.focused()) end },
+   { "Manual", apps.terminal .. " -e man awesome" },
+   { "Edit config", apps.editor_cmd .. " " .. awesome.conffile },
+   { "Restart", awesome.restart },
+   { "Quit", function() awesome.quit() end },
 }
 
-menu.main = awful.menu({
-    items = {
-        { "somewm", menu.somewm, beautiful.awesome_icon },
-        { "open terminal", apps.terminal }
-    }
+menu.favorites = {
+    menu.placeholder,
+    { "   W. I. P."},
+    menu.placeholder,
+}
+
+menu.customize = {
+    menu.placeholder,
+    { "   W. I. P "},
+    menu.placeholder,
+}
+
+menu.power = {
+    { "Poweroff",       function() awful.spawn('loginctl poweroff')             end },
+    { "Reboot",         function() awful.spawn('loginctl reboot')               end },
+    { "Log out",        function() awful.spawn('loginctl kill-session self')    end },
+    { "Lock",           function() awful.spawn('loginctl losk-session self')    end },
+    { "Suspend",        function() awful.spawn('loginctl suspend')              end },
+    { "Hibernate",      function() awful.spawn('loginctl hibernate')            end },
+}
+
+menu.main = freedesktop.menu.build({
+    before = {
+        { "SomeWM"              , menu.somewm, beautiful.awesome_icon },
+        menu.placeholder        ,
+        { "Favorites"           , menu.favorites },
+    },
+    sub_menu = "All Apps",
+    after  = {
+        { "Customize"           , menu.customize },
+        { "Open terminal"       , apps.terminal },
+        menu.placeholder        ,
+        { "Power"               , menu.power },
+    },
 })
 
 return menu
