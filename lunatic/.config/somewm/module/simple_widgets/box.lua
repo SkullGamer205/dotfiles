4d0be6a28a395190b95cc5f2564516f6d8b63295
Akira25 <skullgamer205@disroot.org>
2026-09-01 23:37:08 +1000

    somewm: Update 'simple_widgets.box' module

--- lunatic/.config/somewm/module/simple_widgets/box.lua
+++ lunatic/.config/somewm/module/simple_widgets/box.lua
@@ -22,29 +22,31 @@ local gears     = require('gears')
 -- Simple Widget
 local simplebox = {}
     function simplebox.create_box(w, opts)
+        local widgets = type(w) == "table" and w or { w }
+
         opts                    = opts                  or {}
         opts.on_clicked         = opts.on_clicked       or {}
         
 
         -- Defaults
-        local id                = opts.id                   or nil
-        local inner_margin      = opts.inner_margin         or 2
-        local outer_margin      = opts.outer_margin         or 2
-        local width             = opts.width                or nil
-        local height            = opts.height               or width
+        local id                = opts.id                           or nil
+        local inner_margin      = opts.inner_margin                 or 2
+        local outer_margin      = opts.outer_margin                 or 2
+        local width             = opts.width                        or nil
+        local height            = opts.height                       or width
         
         -- Background
-        local bg_main           = opts.bg_main              or nil
-        local bg_hover          = opts.bg_hover             or bg_main
+        local bg_main           = opts.bg_main                      or nil
+        local bg_hover          = opts.bg_hover                     or bg_main
 
         -- Foreground
         local fg_main           = opts.fg_main  or opts.bg_hover    or nil
         local fg_hover          = opts.fg_hover or opts.bg_main     or nil
 
         -- Actions
-        local action_left       = opts.on_clicked.left      or nil
-        local action_middle     = opts.on_clicked.middle    or nil
-        local action_right      = opts.on_clicked.right     or nil
+        local action_left       = opts.on_clicked.left              or nil
+        local action_middle     = opts.on_clicked.middle            or nil
+        local action_right      = opts.on_clicked.right             or nil
 
         local widget = wibox.widget({
             widget      = wibox.container.margin,
@@ -59,7 +61,8 @@ local simplebox = {}
                 {
                     widget      = wibox.container.margin,
                     margins     = inner_margin,
-                    w,
+                    -- table.unpack(widgets),
+                    widgets,
                 },
             },
             buttons = {
@@ -69,16 +72,14 @@ local simplebox = {}
             }
         })
 
-        if highlight       ~= nil then
-            widget:connect_signal('mouse::enter', function(c)
-                c.bg = bg_hover      
-                c.fg = fg_hover      
-            end)
-            widget:connect_signal('mouse::leave', function(c)
-                c.bg = bg_main      
-                c.fg = fg_main      
-            end)
-        end
+        widget:connect_signal('mouse::enter', function(c)
+            c.bg = bg_hover      
+            c.fg = fg_hover      
+        end)
+        widget:connect_signal('mouse::leave', function(c)
+            c.bg = bg_main      
+            c.fg = fg_main      
+        end)
 
         return widget
     end
