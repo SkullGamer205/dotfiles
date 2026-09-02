4fd611bee0338211c3fa8273fba863e29dab5607
SkullGamer205 <aroslavlevkin86@gmail.com>
2026-09-03 01:07:55 +1000

    somewm: Update 'kbd_layout' widget

--- lunatic/.config/somewm/ui/widgets/kbd/init.lua
+++ lunatic/.config/somewm/ui/widgets/kbd/init.lua
@@ -5,12 +5,30 @@ local beautiful  = require('beautiful')
 local SimpleIcon = require('module.simple_widgets.image').create_icon
 local SimpleBox  = require('module.simple_widgets.box').create_box
 
+-- Keyboard map indicator and switcher
 return function()
-    -- Keyboard map indicator and switcher
-    local current_kbd_layout = awful.widget.keyboardlayout()
+    -- Keyboard icon
     local icon               = SimpleIcon(beautiful.keyboard,{
         main_color           = beautiful.fg_normal,
     })
+
+    -- Keyboard text widget
+    local kbd_widget  = wibox.widget({
+        widget  = wibox.widget.textbox,
+        halign  = 'center',
+        valign  = 'center',
+        markup  = "<i>" .. "N/A" .. "</i>",
+    })
+
+    -- Update function
+    local function update_kbd()
+        local layouts = awful.widget.keyboardlayout.get_groups_from_group_names(awesome.xkb_get_group_names())
+        local current = awesome.xkb_get_layout_group() + 1
+
+        if layouts[current] then
+            kbd_widget.markup = "<i>" .. (layouts[current].file):upper() .. "</i>"
+        end
+    end
 
     local widget             = wibox.widget({
         layout = wibox.layout.fixed.vertical,
@@ -19,9 +37,19 @@ return function()
             widget  = wibox.container.place,
             halign  = 'center',
             valign  = 'center',
-            current_kbd_layout,
+            kbd_widget,
         }
     })
+
+    -- Switch layout
+    local function change_kbd()
+    end
+
+    -- Connect Signal
+    awesome.connect_signal("xkb::map_changed", update_kbd)
+    awesome.connect_signal("xkb::group_changed", update_kbd)
+
+    update_kbd()
 
     return SimpleBox(widget, {
         bg_main = beautiful.colors.background_light,
