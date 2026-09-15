840ab77365e3e4b8fdfb13f5baeadae656d50252
SkullGamer205 <aroslavlevkin86@gmail.com>
2026-09-16 01:46:19 +1000

    somewm: update 'battery' widget
    Make them functional

--- lunatic/.config/somewm/ui/widgets/battery/widget.lua
+++ lunatic/.config/somewm/ui/widgets/battery/widget.lua
@@ -11,13 +11,44 @@ local battery_module  = require('ui.widgets.battery.stats')
 
 local battery         = battery_module({})
 
-local bat_percentage = wibox.widget({
-    widget  = wibox.widget.textbox,
-    halign  = 'center',
-    valign  = 'center',
-    text    = nil,
-    font    = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$') * 2
-})
+local function box_template(opts)
+    opts = opts or {}
+    local icon  = SimpleIcon(opts.icon, {
+         width = opts.width or 32,
+         main_color = opts.color or nil,
+     })
+     
+     local text_widget = wibox.widget({
+         widget  = wibox.widget.textbox,
+         halign  = 'center',
+         valign  = 'center',
+         text    = opts.text or 'N/A',
+         font    = beautiful.font:match('[a-zA-Z ]+') .. (tonumber(beautiful.font:match('%d+$'))) * (opts.font_size or 1)
+     })
+      
+     local box = SimpleBox({icon, text_widget}, {
+         bg_main  = beautiful.colors.background_light,
+         align    = "vertical",
+     })
+     
+     return box, text_widget
+end
+
+local main_box,   main_text   = box_template({ width = 64, font_size = 2, icon = beautiful.battery_full,     text = "N/A%" })
+local health_box, health_text = box_template({ icon = beautiful.battery_health, color = beautiful.fg_normal, text = 'N/A%' })
+local loops_box,  loops_text  = box_template({ icon = beautiful.battery_loops , color = beautiful.fg_normal, text =  'N/A' })
+local volt_box,   volt_text   = box_template({ icon = beautiful.battery_volt  , color = beautiful.fg_normal, text =  'N/A' })
+local watt_box,   watt_text   = box_template({ icon = beautiful.battery_watt  , color = beautiful.fg_normal, text =  'N/A' })
+
+
+-- Helper function to safely update the text inside a box_template
+local function update_text(box_widget, new_text)
+    -- get_children_by_id returns a TABLE. We must grab the first matching widget [1].
+    local text_widget = box_widget:get_children_by_id("text")[1]
+    if text_widget then
+        text_widget.text = new_text
+    end
+end
 
 -- 2. Create the timer ONCE at the module scope
 local battery_timer = gears.timer({
@@ -26,61 +57,16 @@ local battery_timer = gears.timer({
     call_now    = true,
     callback    = function()
         local stats = battery.update()
-        bat_percentage.text = string.format("%s%%", stats.perc)
+        main_text.text   = string.format("%s%%", stats.perc)
+        health_text.text = string.format("%s%%", stats.capacity)
+        loops_text.text  = stats.cycles or "N/A"
+        volt_text.text   = string.format("%.2f", stats.rate_voltage / 1e6)
+        watt_text.text   = string.format("%.2f", stats.watt)
     end
 })
 
-local function main_battery_widget()
-    local bat_icon  = SimpleIcon(beautiful.battery_full, {
-        width = 64,
-    })
-
-    local widget = wibox.widget({
-        layout  = wibox.layout.fixed.vertical,
-        bat_icon,
-        bat_percentage,
-    })
-
-    return SimpleBox(widget, {
-        bg_main  = beautiful.colors.background_light,
-    })
-end
-
 local function secondary_battery_widgets()
-    -- Stat box
-    local function stat_box(opts)
-        opts = opts
-
-        local icon  = SimpleIcon(opts.icon, {
-            width = 32,
-            main_color = beautiful.fg_normal,
-        })
-
-        local text = wibox.widget({
-            widget  = wibox.widget.textbox,
-            halign  = 'center',
-            valign  = 'center',
-            text    = opts.text,
-            font    = beautiful.font:match('[a-zA-Z ]+') .. beautiful.font:match('%d+$')
-        })
-
-        local widget = wibox.widget({
-            layout  = wibox.layout.fixed.vertical,
-            icon,
-            text,
-        })
-
-        return SimpleBox(widget, {
-            bg_main  = beautiful.colors.background_light,
-        })
-    end
-
     -- Stats
-    local health = stat_box({ icon = beautiful.battery_health, text = '100%' })
-    local loops  = stat_box({ icon = beautiful.battery_loops , text =   '-1' })
-    local volt   = stat_box({ icon = beautiful.battery_volt  , text =  '0.0' })
-    local watt   = stat_box({ icon = beautiful.battery_watt  , text = '12.0' })
-
     local widget = wibox.widget({
         layout          = wibox.layout.grid,
         forced_num_cols = 2,
@@ -88,10 +74,10 @@ local function secondary_battery_widgets()
         homogeneous     = true,
         expand          = true,
     })
-    widget:add_widget_at(health, 1, 1)
-    widget:add_widget_at(loops , 1, 2)
-    widget:add_widget_at(volt  , 2, 1)
-    widget:add_widget_at(watt  , 2, 2)
+    widget:add_widget_at(health_box, 1, 1)
+    widget:add_widget_at(loops_box , 1, 2)
+    widget:add_widget_at(volt_box  , 2, 1)
+    widget:add_widget_at(watt_box  , 2, 2)
     
     return widget
 end
@@ -105,7 +91,7 @@ return function()
         border_width    = beautiful.border_width,
         {
             layout  = wibox.layout.fixed.horizontal,
-            main_battery_widget(),
+            main_box,
             secondary_battery_widgets(),
         }
     })
